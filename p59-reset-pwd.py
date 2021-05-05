4dd4c597a6594ad4e44db866322929fe7e4ff61e
JimTang <after062111@byui.edu>
2021-05-05 11:28:58 -0600

    profile setting selenium

--- p59-reset-pwd.py
+++ p59-reset-pwd.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
 import sys, getopt
 
 def main(argv):
-   option = False
+   driver = webdriver.Chrome(executable_path="chromedriver.exe", options=options)
    try:
       opts, args = getopt.getopt(argv,"h")
    except getopt.GetoptError:
@@ -13,30 +14,19 @@ def main(argv):
       
    for opt, arg in opts:
       if opt in ['-h']:
-         option = True    
-         print('option = true')
-         return option
+         driver = webdriver.Remote("http://127.0.0.1:4444/wd/hub", DesiredCapabilities.CHROME, options=options)
+         return driver
+   return driver
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
-
 options = webdriver.ChromeOptions()
-options.headless = main(sys.argv[1:])
+options.headless = False
 options.add_argument(f'user-agent={user_agent}')
-options.add_argument("--window-size=1920,1080")
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--allow-running-insecure-content')
-options.add_argument("--disable-extensions")
-options.add_argument("--proxy-server='direct://'")
-options.add_argument("--proxy-bypass-list=*")
-options.add_argument("--start-maximized")
 options.add_argument('--disable-gpu')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(executable_path="chromedriver.exe", options=options)
-# PATH = "C:\Program Files (x86)\chromedriver.exe"
-# driver = webdriver.Chrome(PATH)
-# driver = webdriver.Chrome()
-driver.maximize_window()
+
+driver = main(sys.argv[1:])
 driver.get("https://public.p59.dev/welcome")
 
 
