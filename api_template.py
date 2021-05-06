e47960e8908fb4be3e7e76f136653943c2384784
brenando <brendon.bainbridge11@gmail.com>
2021-05-06 11:16:15 -0600

    api

--- api_template.py
+++ api_template.py
@@ -1,5 +1,7 @@
 import requests
-
+""" CREATE A USER """
+#Call create user API (from postman)
+# :)
 # specify the function to be carried out
 signupURL = "/api/account/sign-up?otp_check=true"
 
@@ -8,16 +10,24 @@ signupURL = "/api/account/sign-up?otp_check=true"
 
 # set a variable to equal the URL
 pitch59_URL = f"https://public.p59.dev/{signupURL}"
-
 # set arguments that focus the data request
 args = {
-    "parameter" : "value"
-}
-
+        "firstName": "Tony",
+        "lastName": "Stark",
+        "isTesterUser": True,
+        "contactNumber": "(999) 999-9641",
+        "emailId": "testmurdock8@gmail.com",
+        "password": "BetterThanCap10",
+        "zipCode": "84440",
+        "userReferralModel": {
+            "referralEmail": "Shandu@gmail.com"
+        },
+        "otpCode": 9865
+    }
 # request information from endpoint according to listed arguments
-response = requests.get(endpoint, params=args)
-
+response = requests.get(pitch59_URL, params=args)
 # check for successful request
+print(response.status_code) 
 if response.status_code == 200:
     #convert data to a python dictionary
     data_dict = response.json()
