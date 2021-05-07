a9d726fc03622893c6fbdbe91fef181895ff4aba
hotfudgebrownlee <jarenbrownlee@gmail.com>
2021-05-07 16:53:39 -0600

    successful delete, broken create

--- delete_user.py
+++ delete_user.py
@@ -2,7 +2,7 @@ import requests
 """ CREATE A USER """
 # :)
 # specify the function to be carried out
-signupURL = "api/account/sign-up?otp_check=true"
+signupURL = "api/users/349324458266065113/deleteTestUser"
 
 # specify delete url
 
@@ -10,23 +10,15 @@ signupURL = "api/account/sign-up?otp_check=true"
 pitch59_URL = f"https://api.p59.dev/{signupURL}"
 # set arguments that focus the data request
 user = {
-    "firstName": "Tony",
-    "lastName": "Stark",
-    "isTesterUser": True,
-    "contactNumber": "(999) 999-9874",
-    "emailId": "yoloswaggyswaggy@gmail.com",
-    "password": "Thi$Pa55word",
-    "zipCode": "84440",
-    "userReferralModel": {
-        "referralEmail": "Shandu@gmail.com"
-    },
-    "otpCode": 9865
+    # "userID": "349322742934047086",
+    # "isTesterUser": True
 }
 head = {
     "content-type": "application/json",
     "Accept" : "*/*",
     "Accept-Encoding" : "gzip, deflate, br",
-    "Connection" : "keep-alive"
+    "Connection" : "keep-alive",
+    "Authorization" : "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTSUQiOiJEbWZEOE9PM3k0azJIbkp0OWFmVFVuNlBJcGVwN1NENHAvdngvNnhpOHVJZU84Wm1kaHJWa3dxdEsya0I1YkJJIiwiVUlEIjoiQllTVjh5UGlwYk9Hdkk4Y0dVN0lYUmQyL2JHb2pRMElIaFVJSU53L1hlU3RHd2YzMkxuMWIzUXVVUmVyOUkzZiIsIlJJRCI6InRuQVZwYmlKaWlQZlN0UVkybzV1SUJHRWsxWDJHVHpwcWFOb2RHM0VqUGs9Iiwicm9sZSI6IlVzZXIiLCJuYmYiOjE2MjA0Mjc4NDEsImV4cCI6MTYyMDc4Nzg0MSwiaWF0IjoxNjIwNDI3ODQxLCJpc3MiOiJQaXRjaDU5QVBJIiwiYXVkIjoiUGl0Y2g1OVdlYkNsaWVudCJ9.2SIjGTyqVfMsU5V5L3HXE-SEzzlPpX4O4YKGK5Zrn5E"
 }
 # request information from endpoint according to listed arguments
 response = requests.post(pitch59_URL, data=user, headers=head)
