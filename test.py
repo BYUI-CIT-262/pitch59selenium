b0d2aef62aa7fab9e64c2cff9612735fbf588bf7
brenando <brendon.bainbridge11@gmail.com>
2021-05-13 11:06:59 -0600

    clean up

--- test.py
+++
@@ -1,1 +0,0 @@
-print('test test hihi hello34')
--- user.py
+++
@@ -1,7 +0,0 @@
-import names
-# Print (text)
-def simple_user():
-    name= names.get_first_name() + names.get_last_name()
-    print(name)
-    return name
-simple_user()
